96b648c6c3f433ae66a5a9dbdc103cb8ec243caa
tnt <45692264+tuna2134@users.noreply.github.com>
2021-08-22 22:30:41 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,9 +19,17 @@ async def on_ready():
 async def start(ctx):
   e=discord.Embed(title="NATION-SERVER-SYSTEM", description="起動中です...")
   m=await ctx.send(embed=e)
-  subprocess.run(f"C:\Users\ {username}\Desktop\NATiON\bedrock-server-1.17.11.01", shell=True)
+  subprocess.run(f"C:\Users\ {username}\Desktop\NATiON\bedrock-server-1.17.11.01/bedrock_server.exe", shell=True)
   e=discord.Embed(title="NATION-SERVER-SYSTEM", description="起動しました")
   await m.edit(e=embed)
+        
+@bot.command
+async def stop(ctx):
+  e=discord.Embed(title="NATION-SERVER-SYSTEM", description="終了させています")
+  m=await ctx.send(embed=e)
+  subprocess.run("taskkill /im bedrock_server.exe", shell=True)
+  e=discord.Embed(title="NATION-SERVER-SYSTEM", description="終了しました")
+  await m.edit(embed=e)
  
 # shell command実行
 @bot.listen(name="on_message")
